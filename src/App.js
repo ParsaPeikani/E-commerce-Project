bad75caa268ed8a06ec4d119f28e66960e0d0049
Parsa <parsapeikani05@gmail.com>
2022-11-08 23:04:13 -0800

    update our menu-items to navigate withRouter HOC
    DownGrade the React Router dom to be able to use the Switch and history for routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
-
-import { Route, Routes } from 'react-router-dom';
-
-import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component';
+
 const HatsPage = () => (
   <div>
-    <h1>HATS PAGE</h1>
+    <h1>HATS PAGE </h1>
   </div>
 );
 
-class App extends React.Component {
-  render() {
-    return (
-      <div >
-        <Routes>
-          <Route path='/' exact element={ <HomePage/> } />
-          <Route path='/hats' exact element={ <HatsPage/> } />
-        </Routes>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
