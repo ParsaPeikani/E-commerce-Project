4e0645508b2557aa8f8549a371f740142286ee50
Parsa <parsapeikani05@gmail.com>
2022-11-10 19:21:01 -0800

    Styling the Sign-in input

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import FormInput from "../form-input/form-input.component";
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -27,27 +29,28 @@ class SignIn extends React.Component{
     render() {
         return (
             <div className="sign-in">
+                &nbsp;
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input 
+                    <FormInput 
                         name="email" 
                         type="email" 
-                        value={this.state.email} 
-                        onChange={this.handleChange}
+                        value={this.state.email}
+                        label='email'
+                        handleChange={this.handleChange}
                         required 
                     />
-                    <label>Email</label>
 
-                    <input 
+                    <FormInput 
                         name="password" 
                         type="password" 
                         value={this.state.password}
-                        onChange={this.handleChange}
+                        label='password'
+                        handleChange={this.handleChange}
                         required 
                     />
-                    <label>Password</label>
 
                     <input type="submit" value="Submit Form" />
                 </form>
