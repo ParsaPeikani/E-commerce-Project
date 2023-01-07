967c7026bd7a5d3f590345cc27f590034097d78e
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-01-07 13:54:20 -0800

    Adding persistor to our project so that the cart items do not vanish when refreshing the page

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>
 );
 
 reportWebVitals();
-
