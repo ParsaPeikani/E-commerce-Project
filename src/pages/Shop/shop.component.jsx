f4b8575e90f6b4209691aec23e712e08969bdbb3
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-01-08 13:18:53 -0800

    Moving our directory and shop data to its own reducer, creating respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/Shop/shop.component.jsx
+++ src/pages/Shop/shop.component.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import { Route } from "react-router-dom";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import { selectShopCollections } from "../../redux/shop/shop.selector";
+import CollectionsOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionsProps }) => (
-      <CollectionPreview key={id} {...otherCollectionsProps} />
-    ))}
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route
+      exact
+      path={`${match.path}/:collectionId`}
+      component={CollectionPage}
+    />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -5,21 +5,21 @@ const INITIAL_STATE = {
       imageUrl:
         "https://cdn.shopify.com/s/files/1/0866/5968/products/NOK13895_5f6ec7fd-3407-4194-b10d-efd409986a6d_540x.jpg?v=1652815594",
       id: 1,
-      linkUrl: "hats",
+      linkUrl: "shop/hats",
     },
     {
       title: "hoodie",
       imageUrl:
         "https://cdn.shopify.com/s/files/1/0866/5968/products/Ygp7XTVY_720x.jpg?v=1666981416",
       id: 2,
-      linkUrl: "",
+      linkUrl: "shop/jackets",
     },
     {
       title: "CREWNECK",
       imageUrl:
         "https://cdn.shopify.com/s/files/1/0866/5968/products/5aqPJ2MY_1080x.jpg?v=1666723306",
       id: 3,
-      linkUrl: "",
+      linkUrl: "shop/sneakers",
     },
     {
       title: "womens",
@@ -27,7 +27,7 @@ const INITIAL_STATE = {
         "https://cdn.shopify.com/s/files/1/0866/5968/products/UwjmbFZk_1080x.jpg?v=1666751199",
       size: "large",
       id: 4,
-      linkUrl: "",
+      linkUrl: "shop/womens",
     },
     {
       title: "mens",
@@ -35,7 +35,7 @@ const INITIAL_STATE = {
         "https://cdn.shopify.com/s/files/1/0866/5968/products/11KRJFVw_1080x.jpg?v=1666743001",
       size: "large",
       id: 5,
-      linkUrl: "",
+      linkUrl: "shop/mens",
     },
   ],
 };
