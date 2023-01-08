151804a5c7f390ab369b68f8f7142c8194e840a2
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-01-08 14:08:59 -0800

    Adding the Stripe API to our application as well as the fake credit card for testing

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51MO6MbEAvyR2N7zQF3fx853v2K9RgwzHJX8YTkOPPnHkDHmOWqd1Lw4NR3aLt7LNr95oTtdyJYkRCQSBTPYZ4U6L00saVekbic";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Wolf Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://www.pngkit.com/png/detail/142-1425246_wolf-png-logo-transparent-library-tribal-wolf-drawing.png"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
