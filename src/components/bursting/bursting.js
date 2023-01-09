7461183b29c22aa454f800fef9fc1e8bad32c431
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-01-08 23:04:11 -0800

    Removing some half of the bursting effect

--- src/components/bursting/bursting.js
+++ src/components/bursting/bursting.js
@@ -1,12 +1,12 @@
 import mojs from "@mojs/core";
 
-const COLORS = {
-  RED: "#FD5061",
-  YELLOW: "#FFCEA5",
-  BLACK: "#29363B",
-  WHITE: "white",
-  VINOUS: "#A50710",
-};
+// const COLORS = {
+//   RED: "#FD5061",
+//   YELLOW: "#FFCEA5",
+//   BLACK: "#29363B",
+//   WHITE: "white",
+//   VINOUS: "#A50710",
+// };
 
 const burst = new mojs.Burst({
   left: 0,
@@ -25,27 +25,27 @@ const burst = new mojs.Burst({
   },
 });
 
-const burst1 = new mojs.Burst({
-  left: 0,
-  top: 0,
-  count: 27,
-  radius: { 50: 250 },
-  children: {
-    fill: "white",
-    shape: "line",
-    stroke: [COLORS.WHITE, COLORS.VINOUS],
-    strokeWidth: 12,
-    radius: "rand(30, 60)",
-    radiusY: 0,
-    scale: { 1: 0 },
-    pathScale: "rand(.5, 1)",
-    degreeShift: "rand(-360, 360)",
-    isForce3d: true,
-  },
-});
+// const burst1 = new mojs.Burst({
+//   left: 0,
+//   top: 0,
+//   count: 27,
+//   radius: { 50: 250 },
+//   children: {
+//     fill: "white",
+//     shape: "line",
+//     stroke: [COLORS.WHITE, COLORS.VINOUS],
+//     strokeWidth: 12,
+//     radius: "rand(30, 60)",
+//     radiusY: 0,
+//     scale: { 1: 0 },
+//     pathScale: "rand(.5, 1)",
+//     degreeShift: "rand(-360, 360)",
+//     isForce3d: true,
+//   },
+// });
 
 const Burst = document.addEventListener("click", function (e) {
-  burst1.tune({ x: e.pageX, y: e.pageY }).generate().replay();
+  // burst1.tune({ x: e.pageX, y: e.pageY }).generate().replay();
   const coords = { x: e.pageX, y: e.pageY };
   burst.tune(coords).replay();
 });
